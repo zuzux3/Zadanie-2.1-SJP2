c419e61f80c70769ce4893bd43fe8a490a5a9104
zuzux3 <zuzannatarazewicz@gmail.com>
2021-11-08 20:57:27 +0100

    del and add

---
+++ script.py
@@ -0,0 +1,7 @@
+from Triangle import *
+
+angle1 = float(input("Podaj pierwszy kat"))
+angle2 = float(input("Podaj drugi kat"))
+angle3 = float(input("Podaj trzeci kat"))
+
+myTriangle = Triangle(angle1, angle2, angle3)
